2db0b6856e012331441176542de5b230ddea8e3c
María José Rodríguez F <rodriguez.mariajose.f@gmail.com>
2017-05-17 01:45:57 -0400

    ingresar-info

---
+++ assets/js/main.js
@@ -0,0 +1,17 @@
+document.getElementsById('demo').addEventListener("click", function(){
+
+function load(){
+	alert("Hola Mundo")
+};	
+
+function cargadaPagina(){
+	alert("La página está cargada");
+}
+
+function mouseSobre(){
+	alert("Estás sobre mi");
+}
+
+function mouseNoSobre(){
+	alert("No estás sobre mi");
+}
